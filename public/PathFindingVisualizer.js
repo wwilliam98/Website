e7db4cb402ee0f054e58b2a5a628d5fce933c5a3
wwilliam98 <wwilliam1908@gmail.com>
2023-08-07 23:34:44 +0800

    still

--- public/PathFindingVisualizer.js
+++ public/PathFindingVisualizer.js
@@ -1,8 +1,8 @@
 //for JS use ESModules (import ....), for Node use CommonJS (require ....)
-import Node from "/PathFindingNode.js";
-import {create_recursive_divisionMaze, create_recursive_horizontal, create_recursive_vertical, create_perimeterWall} from "./PathFinding/createMaze.js";
-import {BFS, DFS} from "./PathFinding/PathfindingUnweightedAlgorithm.js";
-import {Dijkstra, AStar} from "./PathFinding/PathfindingWeightedAlgorithm.js";
+const Node = require("/PathFindingNode.js")
+const {create_recursive_divisionMaze, create_recursive_horizontal, create_recursive_vertical, create_perimeterWall} = require("./PathFinding/createMaze.js")
+const {BFS, DFS} = require("./PathFinding/PathfindingUnweightedAlgorithm.js")
+const {Dijkstra, AStar} = require("./PathFinding/PathfindingWeightedAlgorithm.js")
 
 // Board attributes
 function Board(height, width){
