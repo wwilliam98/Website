429fd8dfe77f59a706f34ebd438353e41a1d1cde
wwilliam98 <wwilliam1908@gmail.com>
2025-02-23 14:58:20 +0800

    fix padding

--- portfolio/src/Components/ExperienceCard.js
+++ portfolio/src/Components/ExperienceCard.js
@@ -9,61 +9,57 @@ import MysqlLogo from "../Images/MysqlLogo.png"
 
 function ExperienceCard() {
   return (
-    // OK -> Item-center, snap-center
-    // <article className='snap-center flex flex-col flex-shrink-0 rounded-lg items-center space-y-7 w-[80vw] m-5 p-10 bg-[#292929] bg-clip-content shadow-[0_0_40px_gray]'>
-    <article>
-        <div className='w-dvw max-w-md md:max-w-3xl flex-shrink-0 flex flex-col items-center justify-center p-10 bg-[#292929] rounded-2xl shadow-[0_0_10px_gray] md:pd-20'>
-            <motion.img
-                initial={{
-                    y: -100,
-                    opactity: 0,
-                }} 
-                transition={{ duration: 1.2 }}
-                whileInView={{ opacity: 1, y: 0}}
-                viewport={{ once:true }}
-                className='m-5 w-24 h-24 md:w-32 md:h-32 lg:w-32 lg:h-32 rounded-full object-cover object-center'
-                src={STLogo}
-            />
+    <article className='max-w-sm md:max-w-3xl flex-shrink-0 flex flex-col items-center justify-center p-5 bg-[#292929] rounded-2xl shadow-[0_0_10px_gray] md:pd-20'>
+        <motion.img
+            initial={{
+                y: -100,
+                opactity: 0,
+            }} 
+            transition={{ duration: 1.2 }}
+            whileInView={{ opacity: 1, y: 0}}
+            viewport={{ once:true }}
+            className='m-5 w-24 h-24 md:w-32 md:h-32 lg:w-32 lg:h-32 rounded-full object-cover object-center'
+            src={STLogo}
+        />
 
-            <div className='max-w-2xl px-5 md:px-10'>
-                <h4 className='text-3xl sm:text-4xl font-light text-center'>STMicroelectronics</h4>
-                <p className='font-bold text-xl sm:text-2xl mt-1 text-center'>Software Engineer</p>
-                <div className='flex space-x-2 my-1 justify-center'>
-                    <img 
-                        className='h-10 w-10 rounded-full'
-                        src={PythonLogo}
-                        alt="Tech Used"
-                    />
-                    <img 
-                        className='h-10 w-10 rounded-full'
-                        src={PHPLogo}
-                        alt="Tech Used"
-                    />
-                    <img 
-                        className='h-10 w-10 rounded-full'
-                        src={JavascriptLogo}
-                        alt="Tech Used"
-                    />
-                    <img 
-                        className='h-10 w-10 rounded-full'
-                        src={PerlLogo}
-                        alt="Tech Used"
-                    />
-                    <img 
-                        className='h-10 w-10 rounded-full'
-                        src={MysqlLogo}
-                        alt="Tech Used"
-                    />
-                </div>
-                <p className='uppercase py-3 sm:py-5 text-gray-300'>May 2022 - Present</p>
-                <ul className='list-disc space-y-2 ml-5 text-sm md:text-lg text-justify text-gray-400'>
-                    <li>Led the development and implementation of multiple AI-based Computer Vision projects aimed at preventing wafer scrap, utilizing Raspberry Pi, Python, and YOLOv8, and successfully saved 60 wafer lots by automating defect detection and process control.</li>
-                    <li>Developed and Maintained websites using PHP, HTML, Javascript, MySQL for Reticle Assembly Team to fracture data, enabling effective data management and analysis.</li>
-                    <li>Created a system for the HR training team to streamline operator training processes, enhancing efficiency and tracking capabilities.</li>
-                    <li>Migrate excel files to MySQL Database using Python Pandas, which streamlined the data management process and saved the company time and resources.</li>
-                    <li>Using Pandas and Matplotlib to move data from Excel files to a database and show the statistics on the website, making information more organized and accessible.</li>
-                </ul>
+        <div className='max-w-2xl px-5 md:px-10'>
+            <h4 className='text-3xl sm:text-4xl font-light text-center'>STMicroelectronics</h4>
+            <p className='font-bold text-xl sm:text-2xl mt-1 text-center'>Software Engineer</p>
+            <div className='flex space-x-2 my-1 justify-center'>
+                <img 
+                    className='h-10 w-10 rounded-full'
+                    src={PythonLogo}
+                    alt="Tech Used"
+                />
+                <img 
+                    className='h-10 w-10 rounded-full'
+                    src={PHPLogo}
+                    alt="Tech Used"
+                />
+                <img 
+                    className='h-10 w-10 rounded-full'
+                    src={JavascriptLogo}
+                    alt="Tech Used"
+                />
+                <img 
+                    className='h-10 w-10 rounded-full'
+                    src={PerlLogo}
+                    alt="Tech Used"
+                />
+                <img 
+                    className='h-10 w-10 rounded-full'
+                    src={MysqlLogo}
+                    alt="Tech Used"
+                />
             </div>
+            <p className='uppercase py-3 sm:py-5 text-gray-300'>May 2022 - Present</p>
+            <ul className='list-disc space-y-2 ml-5 text-sm md:text-lg text-justify text-gray-400'>
+                <li>Led the development and implementation of multiple AI-based Computer Vision projects aimed at preventing wafer scrap, utilizing Raspberry Pi, Python, and YOLOv8, and successfully saved 60 wafer lots by automating defect detection and process control.</li>
+                <li>Developed and Maintained websites using PHP, HTML, Javascript, MySQL for Reticle Assembly Team to fracture data, enabling effective data management and analysis.</li>
+                <li>Created a system for the HR training team to streamline operator training processes, enhancing efficiency and tracking capabilities.</li>
+                <li>Migrate excel files to MySQL Database using Python Pandas, which streamlined the data management process and saved the company time and resources.</li>
+                <li>Using Pandas and Matplotlib to move data from Excel files to a database and show the statistics on the website, making information more organized and accessible.</li>
+            </ul>
         </div>
     </article>
   )
--- portfolio/src/Components/ExperienceCard2.js
+++ portfolio/src/Components/ExperienceCard2.js
@@ -7,44 +7,42 @@ import HTMLLogo from "../Images/HTMLlogo.jpeg"
 
 function ExperienceCard() {
   return (
-        <article>
-            <div className='w-dvw max-w-md md:max-w-3xl flex-shrink-0 flex flex-col items-center justify-center p-10 bg-[#292929] rounded-2xl shadow-[0_0_10px_gray] md:pd-20'>
-                <motion.img
-                    initial={{
-                        y: -100,
-                        opactity: 0,
-                    }} 
-                    transition={{ duration: 1.2 }}
-                    whileInView={{ opacity: 1, y: 0}}
-                    viewport={{ once:true }}
-                    className='m-5  w-24 h-24 sm:w-32 sm:h-32 lg:w-32 lg:h-32 rounded-full object-cover object-center'
-                    src={OpenSlotPic}
-                />
+        <article className='max-w-sm md:max-w-3xl flex-shrink-0 flex flex-col items-center justify-center p-5 bg-[#292929] rounded-2xl shadow-[0_0_10px_gray] md:pd-20'>
+            <motion.img
+                initial={{
+                    y: -100,
+                    opactity: 0,
+                }} 
+                transition={{ duration: 1.2 }}
+                whileInView={{ opacity: 1, y: 0}}
+                viewport={{ once:true }}
+                className='m-5 w-24 h-24 sm:w-32 sm:h-32 lg:w-32 lg:h-32 rounded-full object-cover object-center'
+                src={OpenSlotPic}
+            />
 
-                <div className='max-w-2xl px-5 md:px-10'>
-                    <h4 className='text-3xl sm:text-4xl font-light text-center'>OpenSlot Inc.</h4>
-                    <p className='font-bold text-xl sm:text-2xl mt-1 text-center'>Quality Assurance</p>
-                    <div className='flex space-x-2 my-1 justify-center'>
-                        <img 
-                            className='h-10 w-10 rounded-full'
-                            src={JavascriptLogo}
-                            alt="Tech Used"
-                        />
-                        <img 
-                            className='h-10 w-10 rounded-full'
-                            src={HTMLLogo}
-                            alt="Tech Used"
-                        />
-                    </div>
-                    <p className='uppercase py-3 sm:py-5 text-gray-300'>Summer 2018, Summer 2019</p>
-                    <ul className='list-disc space-y-2 ml-5 text-sm md:text-lg text-justify text-gray-400'>
-                        <li>Collaborated with a team of developers to identify and document faults in the OpenSlot application, resulting in a 50% reduction in faults.</li>
-                        <li>Participate in code reviews and provide feedback on the quality of the code.</li>
-                        <li>Identified and documented problems with program function, output, and content to improve quality of product.</li>
-                        <li>Designed test plans and procedures with end users which are doctors and dentists by understanding and analyzing functionality of applications to fit business needs.</li>
-                        <li>Proposed feedbacks and recommendations to software developers on usability and functionality to improve application features.</li>
-                    </ul>
+            <div className='max-w-2xl px-5 md:px-10'>
+                <h4 className='text-3xl sm:text-4xl font-light text-center'>OpenSlot Inc.</h4>
+                <p className='font-bold text-xl sm:text-2xl mt-1 text-center'>Quality Assurance</p>
+                <div className='flex space-x-2 my-1 justify-center'>
+                    <img 
+                        className='h-10 w-10 rounded-full'
+                        src={JavascriptLogo}
+                        alt="Tech Used"
+                    />
+                    <img 
+                        className='h-10 w-10 rounded-full'
+                        src={HTMLLogo}
+                        alt="Tech Used"
+                    />
                 </div>
+                <p className='uppercase py-3 sm:py-5 text-gray-300'>Summer 2018, Summer 2019</p>
+                <ul className='list-disc space-y-2 ml-5 text-sm md:text-lg text-justify text-gray-400'>
+                    <li>Collaborated with a team of developers to identify and document faults in the OpenSlot application, resulting in a 50% reduction in faults.</li>
+                    <li>Participate in code reviews and provide feedback on the quality of the code.</li>
+                    <li>Identified and documented problems with program function, output, and content to improve quality of product.</li>
+                    <li>Designed test plans and procedures with end users which are doctors and dentists by understanding and analyzing functionality of applications to fit business needs.</li>
+                    <li>Proposed feedbacks and recommendations to software developers on usability and functionality to improve application features.</li>
+                </ul>
             </div>
         </article>
   )
